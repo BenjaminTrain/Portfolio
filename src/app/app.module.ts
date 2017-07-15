b274b74d1db769a42ac1740e00e850edbf4552a3
BenjaminTrain <benjamin.train@gmail.com>
2017-07-15 13:05:55 +0200

    Page contact

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'contact', component: ContactComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent
+  ],
+  imports: [
+   RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    ),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
