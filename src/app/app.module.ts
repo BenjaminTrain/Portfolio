3861fb2296c55ff5ca536c8f55fb31c4cd1b68a1
BenjaminTrain <benjamin.train@gmail.com>
2017-07-15 19:50:39 +0200

    Diplomes + Experiences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { DiplomesComponent } from './diplomes/diplomes.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperiencesComponent } from './experiences/experiences.component';
+
 
 const appRoutes: Routes = [
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'diplomes', component: DiplomesComponent },
+  { path: 'experiences', component: ExperiencesComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    DiplomesComponent,
+    ExperiencesComponent
   ],
   imports: [
    RouterModule.forRoot(
